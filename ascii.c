faa184aa4be79ba7b630e40da7827f7cb0623654
321prasad0058 <110602321+321prasad0058@users.noreply.github.com>
2022-10-24 10:49:00 +0530

    Printing the ASCII Value of a Character

---
+++ ascii.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+{
+    char c;
+    
+    //Taking Input
+    printf("Enter any character:\n");
+    scanf("%c",&c);
+    
+    // Displays the character as well as its ASCII Value
+    printf("ASCII value of %c is %d",c,c);
+    
+    return 0;
+}
+
+
+/*
+
+Output:
+
+Enter any character:
+Z
+ASCII value of Z is 90
+
+
+*/
