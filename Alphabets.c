0e35807dcfcd315c03d9153f53d0afdd1a3819a8
321prasad0058 <110602321+321prasad0058@users.noreply.github.com>
2022-10-25 09:43:06 +0530

    Printing the English Alphabets using ASCII Values

---
+++ Alphabets.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    char c;
+
+/* Printing the characters from A to Z
+    ASCII Value :- 'A'=65, 'Z'=90      */
+    for(c=65;c<=90;c++)
+      printf("%c\t",c);
+}
+
+/*
+
+Output:
+
+A	B	C	D	E	F	G	H	I	J	K	L	M	N	O	P	Q	R	S	T	U	V	W	X	Y	Z	
+
+*/
