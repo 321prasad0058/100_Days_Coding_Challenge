9bbfe498cee50ea49a626ffc70f32790beb19dc5
321prasad0058 <110602321+321prasad0058@users.noreply.github.com>
2022-12-24 23:50:43 +0530

    Code to Reverse a String using Stack

---
+++ RevStack.c
@@ -0,0 +1,42 @@
+/*
+Code to reverse a String using Stack 
+
+Author : Prasad Anil Alai
+
+*/
+
+#include <stdio.h>  
+#include <string.h>  
+  
+#define max 100  
+int top,stack[max];  
+  
+void push(char x){  
+  
+      // Push(Inserting Element in stack) operation  
+      if(top == max-1){  
+          printf("stack overflow");  
+      }  else {  
+          stack[++top]=x;  
+      }  
+  
+}  
+  
+void pop(){  
+    // Pop (Removing element from stack)  
+      printf("%c",stack[top--]);  
+}  
+  
+  
+void main()  
+{  
+   char str[]="sri lanka";  
+   int len = strlen(str);  
+   int i;  
+  
+   for(i=0;i<len;i++)  
+        push(str[i]);  
+  
+   for(i=0;i<len;i++)  
+      pop();  
+} 
