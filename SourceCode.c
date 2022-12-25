e300f03bebd787d643e316cbd15c6c5c6078071c
321prasad0058 <110602321+321prasad0058@users.noreply.github.com>
2022-12-25 14:09:13 +0530

    Code to display Source Code itself as an Output

---
+++ SourceCode.c
@@ -0,0 +1,24 @@
+/*
+Code to display the Source Code itself as an Output
+
+Author : Prasad Anil Alai
+
+*/
+
+#include <stdio.h>
+int main() {
+    FILE *fp;
+    int c;
+   
+    // open the current input file
+    fp = fopen(__FILE__,"r");
+
+    do {
+         c = getc(fp);   // read character 
+         putchar(c);     // display character
+    }
+    while(c != EOF);  // loop until the end of file is reached
+    
+    fclose(fp);
+    return 0;
+}
